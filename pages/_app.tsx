c3502c63755636c96e02ad0b9d782b746ed74b93
zorro901 <zoro907@gmail.com>
2021-09-02 02:32:00 +0900

    4. React-query with SpaceX GraphQL API

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import '../styles/globals.css'
+import { AppProps } from 'next/app'
+import { useState } from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+function MyApp({Component, pageProps}: AppProps) {
+  const [queryClient] = useState(() => new QueryClient())
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false}/>
+    </QueryClientProvider>
+  )
+}
+
+export default MyApp
---
+++ types/types.ts
@@ -0,0 +1,13 @@
+export interface Rocket{
+      id: string
+      name: string
+      mass: {
+        kg: 30146
+      },
+      height: {
+        meters: number
+      },
+      diameter: {
+        meters: number
+      }
+    }
